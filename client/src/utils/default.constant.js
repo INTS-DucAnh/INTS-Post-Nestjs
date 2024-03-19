0302e80ba904ce82b85656afe156fe0eaea7a8f0
DucAnh2611 <ducanhmc24@gmail.com>
2024-03-19 21:22:17 +0700

    Create constant for reduce hardcode

---
+++ client/src/utils/default.constant.js
@@ -0,0 +1,5 @@
+export const defaultFind = {
+  skip: 0,
+  limit: 10,
+  max: 0,
+};
---
+++ client/src/utils/toast.contstant.js
@@ -0,0 +1,13 @@
+export const toastSevertity = {
+  success: "",
+  error: "",
+  warning: "",
+};
+
+export const toastSummary = {
+  success: "",
+  error: "",
+  warning: "",
+};
+
+export const toastMessage = (message) => `${message}`;
